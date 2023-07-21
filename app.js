1a4ea0524f04c69f1eee3557ecc5be33a824cf0f
Mausoomi Roy <mausoomiroy68613@gmail.com>
2023-07-21 21:09:27 +0530

    fixed cors

--- app.js
+++ app.js
@@ -1,37 +1,47 @@
 import express from "express";
-import userRouter from "./routes/user.js";
-import taskRouter from "./routes/task.js";
-import { config } from "dotenv";
+import userRouter from "./Routes/user.js";
+import taskRouter from "./Routes/task.js";
+import {config} from  "dotenv"
 import cookieParser from "cookie-parser";
-import { errorMiddleWare } from "./middlewares/error.js";
-import cors from "cors";
 
+import { errorMiddleware } from "./middlewares/error.js";
+import cors from "cors"
 
 export const app = express();
 
 config({
-  path: "./data/config.env",
-});
+  path: "./data/config.env"
+})
 
-//using middlewares
-app.use(
-  cors({
-    origin: 'http://localhost:5173',
-    methods: ["GET", "POST", "PUT", "DELETE"],
-    credentials: true,
-  })
-);
+
+// app.use(cors({
+//   origin:[process.env.FRONTEND_URL],
+//   methods :["GET","PUT","POST","DELETE"],
+//   credentials:true
+// }));
+const frontendURL = process.env.FRONTEND_URL;
+
+//Using Middlewares
+app.use(cors({
+  origin: function (origin, callback) {
+    if (origin === frontendURL) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
+  credentials: true // Enable sending cookies in the request (if needed)
+}));
 app.use(express.json());
-app.use(cookieParser());
+app.use (cookieParser());
 
-
-//Using routes
+//Using Routes
 app.use("/api/v1/users", userRouter);
 app.use("/api/v1/task", taskRouter);
 
 app.get("/", (req, res) => {
-  res.send("Nice Working");
+  res.send("Nice working");
 });
 
 //Using Error Middleware
-app.use(errorMiddleWare);
+app.use(errorMiddleware)
