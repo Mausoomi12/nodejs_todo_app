9fc7853f34bb4879d87efd353d030583e5790648
Mausoomi Roy <mausoomiroy68613@gmail.com>
2023-07-21 11:56:51 +0530

    undo

--- server.js
+++ server.js
@@ -3,12 +3,6 @@ import { connectDB } from "./data/database.js";
 
 
 connectDB();
-app.use(function(req, res, next) {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST','PUT','DEELETE');
-    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type, Authorization');
-    next();
-});
 
 app.listen(process.env.PORT,()=>{
     console.log(`Server is Working on port:${process.env.PORT} in ${process.env.NODE_ENV} Mode`
